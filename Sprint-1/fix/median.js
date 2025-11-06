481a8ef6155e6830d31225c2aced7438d2b127d6
Pythia <el1vas@yahoo.gr>
2025-11-06 17:04:52 +0100

    Adjusted the function for the case that list length is even or odd

--- Sprint-1/fix/median.js
+++ Sprint-1/fix/median.js
@@ -6,9 +6,12 @@
 // or 'list' has mixed values (the function is expected to sort only numbers).
 
 function calculateMedian(list) {
-  const middleIndex = Math.floor(list.length / 2);
-  const median = list.splice(middleIndex, 1)[0];
-  return median;
+    const middleIndex = Math.floor(list.length / 2);
+    const median = list.splice(middleIndex, 1)[0];
+    if (list.length % 2 === 0) {
+        return list[middleIndex - 1] + list[middleIndex] / 2;
+    }
+    return median;
 }
 
 module.exports = calculateMedian;
