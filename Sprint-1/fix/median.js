6bd6d024a0731f9c9f28b9339d0a35c91e4d7a6c
Pythia <el1vas@yahoo.gr>
2025-11-06 17:12:30 +0100

    Fixed typos and moved the splice after the if calculation

--- Sprint-1/fix/median.js
+++ Sprint-1/fix/median.js
@@ -7,10 +7,10 @@
 
 function calculateMedian(list) {
     const middleIndex = Math.floor(list.length / 2);
-    const median = list.splice(middleIndex, 1)[0];
     if (list.length % 2 === 0) {
-        return list[middleIndex - 1] + list[middleIndex] / 2;
+        return (list[middleIndex - 1] + list[middleIndex]) / 2;
     }
+    const median = list.splice(middleIndex, 1)[0];
     return median;
 }
 
