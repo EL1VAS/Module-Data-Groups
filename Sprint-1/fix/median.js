565575a5e1b74ce496ce79eb570de55120b5176c
Pythia <el1vas@yahoo.gr>
2025-11-06 17:59:37 +0100

    Moved the sorted and middleIndex variables on top of the function for better execution

--- Sprint-1/fix/median.js
+++ Sprint-1/fix/median.js
@@ -6,6 +6,8 @@
 // or 'list' has mixed values (the function is expected to sort only numbers).
 
 function calculateMedian(list) {
+    const sorted = [...list].sort((a, b) => a - b); //Sorted the list ascending
+    const middleIndex = Math.floor(sorted.length / 2); //Find the middle index by dividing the length of sorted list in 2
     for (let i = 0; i < list.length; i++) {
         //Created a loop to check all list's indexes to find a number
         if (typeof list(i) === "number") {
@@ -14,8 +16,6 @@ function calculateMedian(list) {
     }
     return null; //If loop finished and there is no number
 
-    const sorted = [...list].sort((a, b) => a - b); //Sorted the list ascending
-    const middleIndex = Math.floor(sorted.length / 2); //Find the middle index by dividing the length of sorted list in 2
     if (sorted.length % 2 === 0) {
         // If length of list is even
         return (sorted[middleIndex - 1] + sorted[middleIndex]) / 2; //Find the mean of the two digits in the middle
