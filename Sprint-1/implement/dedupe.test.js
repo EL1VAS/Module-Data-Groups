56af47ef4f9d6f044aca97917ce782294b572e5d
Pythia <el1vas@yahoo.gr>
2025-11-06 22:44:31 +0100

    Wrote the test for the empty array

--- Sprint-1/implement/dedupe.test.js
+++ Sprint-1/implement/dedupe.test.js
@@ -16,7 +16,12 @@ E.g. dedupe([1, 2, 1]) target output: [1, 2]
 // Given an empty array
 // When passed to the dedupe function
 // Then it should return an empty array
-test.todo("given an empty array, it returns an empty array");
+test("given an empty array, it returns an empty array", () => {
+    const empty = [];
+    const currentOutput = dedupe(empty);
+    const targetOutput = [];
+    expect(currentOutput).toEqual(targetOutput);
+});
 
 // Given an array with no duplicates
 // When passed to the dedupe function
