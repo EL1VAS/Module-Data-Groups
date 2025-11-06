0d09f79a62373212482d6e0bce9599c34c8753ed
Pythia <el1vas@yahoo.gr>
2025-11-06 14:03:38 +0100

    Wrote the function to swap first-last digits of array

--- prep/exercise-2.js
+++ prep/exercise-2.js
@@ -1,7 +1,13 @@
 // Write a function which takes an array as a parameter
 //   and swaps the first element with the last element
 
-function swapFirstAndLast(arr) {}
+function swapFirstAndLast(arr) {
+    let arr = []; //Made arr an array
+    const first = arr[0]; //Indivates the first digit location of the array
+    const last = arr[arr.length - 1]; //Indicates the lst digit location of the array
+    arr[0] = last; //Because I can't reassign a new value to a const and we want the first position to hold the value of the last
+    arr[arr.length - 1] = first;
+}
 
 const myArray = [5, 2, 3, 4, 1];
 swapFirstAndLast(myArray);
