4a38ce95b7a9e2ea387638cc1ddab8634996cef2
Pythia <el1vas@yahoo.gr>
2025-11-06 14:04:34 +0100

    Deleted the arr[] as testing indicated it was already declared

--- prep/exercise-2.js
+++ prep/exercise-2.js
@@ -2,7 +2,6 @@
 //   and swaps the first element with the last element
 
 function swapFirstAndLast(arr) {
-    let arr = []; //Made arr an array
     const first = arr[0]; //Indivates the first digit location of the array
     const last = arr[arr.length - 1]; //Indicates the lst digit location of the array
     arr[0] = last; //Because I can't reassign a new value to a const and we want the first position to hold the value of the last
