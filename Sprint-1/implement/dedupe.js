1de0bfba4d7025e5a8880de1202cf5ce33c73001
Pythia <el1vas@yahoo.gr>
2025-11-07 14:13:31 +0100

    Added comments

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,15 +1,18 @@
 function dedupe(list) {
     if (list.length === 0) {
-        return [];
+        //If array is empty
+        return []; //returns empty array
     }
 
-    const onlyOnce = [];
+    const onlyOnce = []; //Empty array for adding the items in array found for the first time
     for (let i = 0; i < list.length; i++) {
+        //loop
         if (onlyOnce.indexOf(list[i]) === -1) {
-            onlyOnce.push(list[i]);
+            //If in onlyOnce the item of the location i is not included
+            onlyOnce.push(list[i]); //Adds the item in location i to the onlyOnce array
         }
     }
-    return onlyOnce;
+    return onlyOnce; //Array with unique items
 }
 
 module.exports = dedupe;
