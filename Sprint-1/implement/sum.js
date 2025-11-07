02e4a66ac5ca7ffa082df8a00be396437f24431b
Pythia <el1vas@yahoo.gr>
2025-11-07 13:43:34 +0100

    Wrote the function to add each item of the array to the previous if not empty

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,12 @@
 function sum(elements) {
+    if (elements.length === 0) {
+        return 0;
+    }
+    let total = 0;
+    for (let i = 0; i < elements.length; i++) {
+        total += elements[i];
+    }
+    return total;
 }
 
 module.exports = sum;
