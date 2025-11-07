94d1a7d1374a0b6b896cd1ed41a0f0791b291fa0
Pythia <el1vas@yahoo.gr>
2025-11-07 11:58:20 +0100

    Fixed typo

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -2,8 +2,9 @@ function dedupe(list) {
     if (list.length === 0) {
         return [];
     }
+
     const onlyOnce = [];
-    for (let i = 0; i < list.length, i++; ) {
+    for (let i = 0; i < list.length; i++; ) {
         if (onlyOnce.indexOf(list[i]) === -1) {
             onlyOnce.push(list[i]);
         }
