284757b451abbe1f2e70377197575267fc5ea241
Pythia <el1vas@yahoo.gr>
2025-11-07 12:26:14 +0100

    Wrote test for one number array

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,6 +1,6 @@
 function findMax(elements) {
     if (elements.length === 0) {
-        //If list is empty
+        //If array is empty
         return -Infinity;
     }
     return Math.max(elements); //Mathematical max of the elements value
