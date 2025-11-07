88d9ab2ed697a87f3170e490a43ec3f11ec5689c
Pythia <el1vas@yahoo.gr>
2025-11-07 11:53:51 +0100

    Tried to write the function to pass the rest of tests

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -2,6 +2,13 @@ function dedupe(list) {
     if (list.length === 0) {
         return [];
     }
+    const onlyOnce = [];
+    for (let i = 0; i < list.length, i++; ) {
+        if (onlyOnce.indexOf(list[i]) === -1) {
+            onlyOnce.push(list[i]);
+        }
+    }
+    return onlyOnce;
 }
 
 module.exports = dedupe;
