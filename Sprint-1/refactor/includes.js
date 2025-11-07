fc17d7aca98d601c884f8ce0b58965b00ca76f11
Pythia <el1vas@yahoo.gr>
2025-11-07 16:20:30 +0100

    Refactored the function

--- Sprint-1/refactor/includes.js
+++ Sprint-1/refactor/includes.js
@@ -1,13 +1,13 @@
 // Refactor the implementation of includes to use a for...of loop
 
 function includes(list, target) {
-  for (let index = 0; index < list.length; index++) {
-    const element = list[index];
-    if (element === target) {
-      return true;
+    for (let index = 0; index < list.length; index++) {
+        const element = list[index];
+        if (element !== target) {
+            return false;
+        }
     }
-  }
-  return false;
+    return true;
 }
 
 module.exports = includes;
