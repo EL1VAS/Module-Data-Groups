7f946f0aa28bf63f79688f90c563c1f100bd7bb6
Pythia <el1vas@yahoo.gr>
2025-11-13 17:39:17 +0100

    Predicted the log

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -4,12 +4,14 @@
 // but it isn't working...
 // Fix anything that isn't working
 
+//I think it will log housenumber: 42 instead of 42.
+
 const address = {
-  houseNumber: 42,
-  street: "Imaginary Road",
-  city: "Manchester",
-  country: "England",
-  postcode: "XYZ 123",
+    houseNumber: 42,
+    street: "Imaginary Road",
+    city: "Manchester",
+    country: "England",
+    postcode: "XYZ 123",
 };
 
 console.log(`My house number is ${address[0]}`);
