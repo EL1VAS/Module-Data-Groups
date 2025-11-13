7afd34fb1e195b7fc7279da33c1188ff2425fac9
Pythia <el1vas@yahoo.gr>
2025-11-13 13:34:56 +0100

    Changed the function to stop early if there is an empty string

--- prep/parse-query-string.js
+++ prep/parse-query-string.js
@@ -1,9 +1,13 @@
 function parseQueryString(queryString) {
     const queryParams = {};
+    if (queryString.length === 0) {
+        return queryParams;
+    }
 
-    const [key, value] = queryString.split("="); // key will hold 'fruit', value will hold 'banana'
-    queryParams[key] = value;
+    const [key, value] = queryString.split("="); // will hold ['fruit', 'banana']
+    queryParams[key] = value; // will set the property name with the value of the key variable
 
     return queryParams;
 }
+
 module.exports = parseQueryString;
