09399111f9f97995e1b76be600a80c62c27f95a8
Pythia <el1vas@yahoo.gr>
2025-11-13 12:58:52 +0100

    Added the import

--- prep/parse-query-string.test.js
+++ prep/parse-query-string.test.js
@@ -1,3 +1,5 @@
+const parseQueryString = require("./parse-query-string");
+
 test("given a query string with no query parameters, returns an empty object", () => {
     const input = "";
     const currentOutput = parseQueryString(input);
