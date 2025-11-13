4855f420a234db2515eb7393e3cdb33720f44b35
Pythia <el1vas@yahoo.gr>
2025-11-13 13:00:53 +0100

    Added test for input to return object

--- prep/parse-query-string.test.js
+++ prep/parse-query-string.test.js
@@ -7,3 +7,11 @@ test("given a query string with no query parameters, returns an empty object", (
 
     expect(currentOutput).toEqual(targetOutput);
 });
+
+test("given a query string with one pair of query params, returns them in object", () => {
+    const input = "fruit=banana";
+    const currentOutput = parseQueryString(input);
+    const targetOutput = { fruit: "banana" };
+
+    expect(currentOutput).toEqual(targetOutput);
+});
