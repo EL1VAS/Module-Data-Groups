7faf6c52dd21b9165972abc181ce9f2d30f310c8
Pythia <el1vas@yahoo.gr>
2025-11-13 13:07:07 +0100

    Change the code to pass the second test. Splted the string into pair of key and value.

--- prep/parse-query-string.js
+++ prep/parse-query-string.js
@@ -1,5 +1,10 @@
-function parseQueryString() {
-    return {};
+function parseQueryString(queryString) {
+    const queryParams = {};
+    const keyValuePair = queryString.split("=");
+    const key = keyValuePair[0];
+    const value = keyValuePair[1];
+    queryParams.key = value;
+    return queryParams;
 }
 
 module.exports = parseQueryString;
