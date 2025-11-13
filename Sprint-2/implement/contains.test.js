d8631dda14f95922e4685c2f40a5ecd7a3768a47
Pythia <el1vas@yahoo.gr>
2025-11-13 20:04:42 +0100

    Wrote test for object and property name and test for empty object

--- Sprint-2/implement/contains.test.js
+++ Sprint-2/implement/contains.test.js
@@ -16,11 +16,21 @@ as the object doesn't contains a key of 'c'
 // Given a contains function
 // When passed an object and a property name
 // Then it should return true if the object contains the property, false otherwise
+test("when passed an object and a property name, returns true if the object contains the property", () => {
+    const currentOutput = contains({ a: 1, b: 2 }, "a");
+    const targetOutput = true;
+    expect(currentOutput).toEqual(targetOutput);
+});
 
 // Given an empty object
 // When passed to contains
 // Then it should return false
-test.todo("contains on empty object returns false");
+test("contains on empty object returns false", () => {
+    const empty = {};
+    const currentOutput = contains(empty, "");
+    const targetOutput = false;
+    expect(currentOutput).toEqual(targetOutput);
+});
 
 // Given an object with properties
 // When passed to contains with an existing property name
