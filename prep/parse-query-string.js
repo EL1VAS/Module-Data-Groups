741525e9c4601b17b1451aed40161c22bdafabe6
Pythia <el1vas@yahoo.gr>
2025-11-13 14:22:22 +0100

    Changed the function for multiple pairs

--- prep/parse-query-string.js
+++ prep/parse-query-string.js
@@ -1,12 +1,10 @@
 function parseQueryString(queryString) {
     const queryParams = {};
-    if (queryString.length === 0) {
-        return queryParams;
+    const keyValuePairs = queryString.split("&");
+    for (const pair of keyValuePairs) {
+        const [key, value] = pair.split("=");
+        queryParams[key] = value;
     }
-
-    const [key, value] = queryString.split("="); // will hold ['fruit', 'banana']
-    queryParams[key] = value; // will set the property name with the value of the key variable
-
     return queryParams;
 }
 
