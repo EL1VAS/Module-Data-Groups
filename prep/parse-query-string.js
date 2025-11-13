b6bc558213bf3836d4c2e230f51cb57ef2e02922
Pythia <el1vas@yahoo.gr>
2025-11-13 13:12:04 +0100

    Different expression of same function

--- prep/parse-query-string.js
+++ prep/parse-query-string.js
@@ -1,10 +1,9 @@
 function parseQueryString(queryString) {
     const queryParams = {};
-    const keyValuePair = queryString.split("=");
-    const key = keyValuePair[0];
-    const value = keyValuePair[1];
+
+    const [key, value] = queryString.split("="); // key will hold 'fruit', value will hold 'banana'
     queryParams[key] = value;
+
     return queryParams;
 }
-
 module.exports = parseQueryString;
