5fd7c0db77ad25153b60e4ff50a42258948f1091
Pythia <el1vas@yahoo.gr>
2025-11-13 22:26:23 +0100

    Created the function by checking the array with loop and add its pairs as a key:value to object lookup

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,5 +1,12 @@
-function createLookup() {
-  // implementation here
+function createLookup(arrayPairs) {
+    const lookup = {};
+    for (let i = 0; i < arrayPairs.length; i++) {
+        //Goes looping in the array
+        const countryCode = arrayPairs[i][0]; //Define that countryCode is the index 0 of the pair
+        const currencyCode = arrayPairs[i][1]; //Define that currencyCode is the index 1 of the pair
+        lookup[countryCode] = currencyCode; //Added the key countryCode to lookup with value as currencyCode
+    }
+    return lookup; //Return the new object
 }
 
 module.exports = createLookup;
