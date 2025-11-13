43aa2eb7a73961a07db3352bacf8c32d8071482c
Pythia <el1vas@yahoo.gr>
2025-11-13 12:50:45 +0100

    Wrote the first test for empty string with no query parameters

---
+++ prep/parse-query-string.test.js
@@ -0,0 +1,7 @@
+test("given a query string with no query parameters, returns an empty object", () => {
+    const input = "";
+    const currentOutput = parseQueryString(input);
+    const targetOutput = {};
+
+    expect(currentOutput).toEqual(targetOutput);
+});
