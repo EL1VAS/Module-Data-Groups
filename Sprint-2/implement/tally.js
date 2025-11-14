8d935b85a8994a22770b47a7bd5ccecade5dc33d
Pythia <el1vas@yahoo.gr>
2025-11-14 17:28:25 +0100

    Wrote the tally function by creating an empty object and adding +1 everytime the loop encounters the item

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,13 @@
-function tally() {}
+function tally(items) {
+    const countOfItems = {};
+    for (let i = 0; i < items.length; i++) {
+        if (countOfItems[items[i]]) {
+            countOfItems[items[i]] += 1;
+        } else {
+            countOfItems[items[i]] = 1;
+        }
+    }
+    return countOfItems;
+}
 
 module.exports = tally;
