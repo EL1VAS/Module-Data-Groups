4ab9282f62a03e03f4b4916c3e08f13b8e2f68e1
Pythia <el1vas@yahoo.gr>
2025-11-14 22:03:59 +0100

    Added comments

--- Sprint-2/interpret/invert.test.js
+++ Sprint-2/interpret/invert.test.js
@@ -6,5 +6,6 @@ const invert = require("./invert.js");
 // Test for object with one pair key value
 
 test("when a one pair object is passed to invert, returns the object with swapped key value", () => {
-    expect(invert({ a: 1 })).toEqual({ 1: "a" });
+    expect(invert({ a: 1 })).toEqual({ 1: "a" }); //Found out that keys in objects are always strings, so it has to return a string "a".
+    //Writting 1:"a" means that since 1 in now the key is already turned to a string
 });
