808f517b53400c739ca65d8bb017f590b9f1b9f1
Pythia <el1vas@yahoo.gr>
2025-11-14 15:48:44 +0100

    Added notes

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -1,15 +1,15 @@
 function parseQueryString(queryString) {
-    const queryParams = {};
+    const queryParams = {}; //
     if (queryString.length === 0) {
         return queryParams;
     }
-    const keyValuePairs = queryString.split("&");
+    const keyValuePairs = queryString.split("&"); // Split the string in pairs.
 
     for (const pair of keyValuePairs) {
-        const firstEqualIndex = pair.indexOf("=");
-        const key = pair.slice(0, firstEqualIndex);
-        const value = pair.slice(firstEqualIndex + 1);
-        queryParams[key] = value;
+        const firstEqualIndex = pair.indexOf("="); //First occurance of =
+        const key = pair.slice(0, firstEqualIndex); //Slice on the left of first =
+        const value = pair.slice(firstEqualIndex + 1); //Slice on the right of first =
+        queryParams[key] = value; //Adding the key and value to the object
     }
 
     return queryParams;
