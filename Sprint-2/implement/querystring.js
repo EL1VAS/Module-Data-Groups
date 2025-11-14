8b9b298fd7f59b255e1478ecbf143fc24b97efd9
Pythia <el1vas@yahoo.gr>
2025-11-14 15:44:21 +0100

    Change the code to include cases with more than one "=". Used the pair to define the key and value by separeting in two parts the string using the 1st "=".

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -1,16 +1,18 @@
 function parseQueryString(queryString) {
-  const queryParams = {};
-  if (queryString.length === 0) {
+    const queryParams = {};
+    if (queryString.length === 0) {
+        return queryParams;
+    }
+    const keyValuePairs = queryString.split("&");
+
+    for (const pair of keyValuePairs) {
+        const firstEqualIndex = pair.indexOf("=");
+        const key = pair.slice(0, firstEqualIndex);
+        const value = pair.slice(firstEqualIndex + 1);
+        queryParams[key] = value;
+    }
+
     return queryParams;
-  }
-  const keyValuePairs = queryString.split("&");
-
-  for (const pair of keyValuePairs) {
-    const [key, value] = pair.split("=");
-    queryParams[key] = value;
-  }
-
-  return queryParams;
 }
 
 module.exports = parseQueryString;
