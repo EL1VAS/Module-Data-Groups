42fe69d7a6697656a86e5d70594bab2d2908adf4
Pythia <el1vas@yahoo.gr>
2025-11-15 13:03:14 +0100

    Added more detailed comments

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,13 +1,14 @@
 function tally(items) {
     if (!Array.isArray(items)) {
-        throw new Error("Input must be an array");
+        throw new Error("Input must be an array"); // I add this for the case that items are not an array
     }
-    const countOfItems = {};
+    const countOfItems = {}; // Create empty object to add the count of items
     for (let i = 0; i < items.length; i++) {
+        // Loop to check each value within the item if it is repeating
         if (countOfItems[items[i]]) {
-            countOfItems[items[i]] += 1;
+            countOfItems[items[i]] += 1; // If indeed the value in item is repeated we add +1
         } else {
-            countOfItems[items[i]] = 1;
+            countOfItems[items[i]] = 1; // If not repeated and it is found for the first time we add 1
         }
     }
     return countOfItems;
