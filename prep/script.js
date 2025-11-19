13043fcf61c86f7d73013ffdcd232531baa405f0
Pythia <el1vas@yahoo.gr>
2025-11-19 15:14:53 +0100

    Added the keyup and addEventListener

--- prep/script.js
+++ prep/script.js
@@ -3,3 +3,11 @@ const remainingCharacters = textarea.maxLength - textarea.value.length;
 
 const charactersLeftP = document.querySelector("#character-limit-info");
 charactersLeftP.textContent = `You have ${remainingCharacters} characters remaining`;
+
+function updateCharacterLimit() {
+    console.log(
+        "keyup event has fired... The browser called updateCharacterLimit..."
+    );
+}
+
+textarea.addEventListener("keyup", updateCharacterLimit);
