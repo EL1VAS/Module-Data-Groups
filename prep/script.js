2da3276cd097521c1946868794dfa463efe8e612
Pythia <el1vas@yahoo.gr>
2025-11-19 15:22:09 +0100

    Fixed typo and used the function before we declare it

--- prep/script.js
+++ prep/script.js
@@ -1,8 +1,5 @@
 const textarea = document.querySelector("textarea");
-const remainingCharacters = textarea.maxLength - textarea.value.length;
-
-const charactersLeftP = document.querySelector("#character-limit-info");
-charactersLeftP.innerText = `You have ${remainingCharacters} characters remaining`;
+updateCharacterLimit();
 
 function updateCharacterLimit() {
     const remainingCharacters = textarea.maxLength - textarea.value.length;
