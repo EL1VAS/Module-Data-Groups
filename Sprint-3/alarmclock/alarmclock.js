be570abb6eb21a09c8da4c77afa63297f64d2b4c
Pythia <el1vas@yahoo.gr>
2025-11-20 22:17:40 +0100

    Created the interval function to create a countdown every second that passes till 00:00. Added comments

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,18 +1,29 @@
 function formatTime(totalSeconds) {
     const minutes = Math.floor(totalSeconds / 60);
     const seconds = totalSeconds % 60;
-    const mm = String(minutes).padStart(2, "0"); //Added padding to cover the chance the input is a single number
-    const ss = String(seconds).padStart(2, "0"); //Also I wanted to match the format of the Time remaining
+    const mm = String(minutes).padStart(2, "0"); // Added padding to cover the chance the input is a single number
+    const ss = String(seconds).padStart(2, "0"); // Also I wanted to match the format of the Time remaining
 
     return `${mm}:${ss}`;
 }
 
 function setAlarm() {
-    const inputValue = document.getElementById("alarmSet").value;
-    const formatedInput = formatTime(inputValue);
-    console.log(formatedInput);
-    const timeRemaining = document.getElementById("timeRemaining");
-    timeRemaining.innerText = `Time Remaining: ${formatedInput}`;
+    const inputValue = document.getElementById("alarmSet").value; // Get the value that we input in the box
+    const formatedInput = formatTime(inputValue); // Use function above to format the number in the input to mm:ss format
+    let remainingSeconds = inputValue; // Added after to be used in the interval function
+    const timeRemaining = document.getElementById("timeRemaining"); // Get to the element of the header Time Remaining
+    timeRemaining.innerText = `Time Remaining: ${formatedInput}`; // And change its text to display the formated input
+    const intervalId = setInterval(function () {
+        // Function that will be repeated every second
+        remainingSeconds -= 1; // And will deduct a second everytime the function runs
+        timeRemaining.innerText = `Time Remaining: ${formatTime(
+            remainingSeconds
+        )}`; // And will show a countdown display in the header
+        if (remainingSeconds <= 0) {
+            // When countdown reaches 00:00
+            clearInterval(intervalId); // Function will stop running
+        }
+    }, 1000);
 }
 
 // DO NOT EDIT BELOW HERE
