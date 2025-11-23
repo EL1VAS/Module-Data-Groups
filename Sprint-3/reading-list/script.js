4ba3b79e130c6ef9ff2651ef7f123a80c9db010a
Pythia <el1vas@yahoo.gr>
2025-11-23 22:35:16 +0100

    testing the css that is loading correct because the test for red color is not passing

--- Sprint-3/reading-list/script.js
+++ Sprint-3/reading-list/script.js
@@ -28,8 +28,10 @@ function createBookBox(books) {
         const li = document.createElement("li"); // Creates an element li
         if (book.alreadyRead === true) {
             li.classList.add("green"); // Adding class to element li
+            li.style.backgroundColor("green");
         } else {
             li.classList.add("red");
+            li.style.backgroundColor("red");
         }
         const container = document.createElement("div"); // Created a div element for all info of our book
         container.className = "book-container"; // Adds div a class for css to use
