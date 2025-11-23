42788fe12c735cfe1cc5c706a0789b0a2a406c62
Pythia <el1vas@yahoo.gr>
2025-11-23 19:59:56 +0100

    Wrote a function to insert dynamically elements in our list

--- Sprint-3/reading-list/script.js
+++ Sprint-3/reading-list/script.js
@@ -22,4 +22,37 @@ const books = [
 ];
 
 const readingList = document.getElementById("reading-list");
-function createBookBox(book) {}
+function createBookBox(books) {
+    for (let i = 0; i < books.length; i++) {
+        const book = books[i]; // Checks each book in the array
+        const li = document.createElement("li"); // Creates an element li
+        if ((book.alreadyRead = true)) {
+            li.classList.add("green"); // Adding class to element li
+        } else {
+            li.classList.add("red");
+        }
+        const container = document.createElement("div"); // Created a div element for all info of our book
+        container.className = "book-container"; // Adds div a class for css to use
+        const textDiv = document.createElement("div"); // Created a div for all text elements
+        textDiv.className = "book-text"; // Gave it a class for css styling
+        const imgDiv = document.createElement("div"); // Created another div just for the image
+        imgDiv.className = "book-image"; // Added class for css
+        const title = document.createElement("h1"); // Created element for our title
+        title.textContent = books.title; // Gave the element a text content of the title taken from our array of books
+        const author = document.createElement("p"); // Created element for author
+        author.textContent = books.author; // Gave the element text matching with the author from our book[i] in array
+        const bookImage = document.createElement("img"); // Created an img element for just our book cover photo
+        bookImage.src = books.bookCoverImage; // Matched it with our book[i] cover image
+
+        textDiv.appendChild(title); // Inserted in our book-text container the title and
+        textDiv.appendChild(author); // the author
+        imgDiv.appendChild(bookImage); // Inserted in our imgDiv container the image
+
+        container.appendChild(textDiv); // Inserted the book-text container into our main container
+        container.appendChild(imgDiv); // Inserted the book-image container into our main container
+
+        li.appendChild(container); // Inserted the main container into our li element
+
+        readingList.appendChild(li); // Added the li element into our readingList
+    }
+}
