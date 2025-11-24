19f181310689c58000fb7fb78502f5fb2f8da27a
Pythia <el1vas@yahoo.gr>
2025-11-24 22:14:47 +0100

    Wrote the function to detect index and move forward or backwards. Added the click events

--- Sprint-3/slideshow/slideshow.js
+++ Sprint-3/slideshow/slideshow.js
@@ -4,5 +4,23 @@ const images = [
     "./assets/cute-cat-c.jpg",
 ];
 
-
 // Write your code here
+let currentIndex = 0;
+function moveFrontOnce(forward) {
+    if (forward) {
+        currentIndex = (currentIndex + 1) % images.length; // If forward is true adds 1 to the index
+        // and wraps with % images.length in case we are in the last index
+    } else {
+        currentIndex = (currentIndex - 1 + images.length) % images.length; // Same here if backward it deducts one index
+        //making sure it wraps back to last image in case we were at index[0]
+    }
+    const image = document.getElementById("carousel-img");
+    image.src = images[currentIndex];
+}
+
+document
+    .getElementById("forward-btn")
+    .addEventListener("click", () => moveFrontOnce(true));
+document
+    .getElementById("backward-btn")
+    .addEventListener("click", () => moveFrontOnce(false));
