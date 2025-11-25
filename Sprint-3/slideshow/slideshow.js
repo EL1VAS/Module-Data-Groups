85d7cb114ed668dc4f945563c66a2b19390a73a4
Pythia <el1vas@yahoo.gr>
2025-11-25 15:31:02 +0100

    Created the functions for auto slideshow and stop. Enabled/disabled the buttons

--- Sprint-3/slideshow/slideshow.js
+++ Sprint-3/slideshow/slideshow.js
@@ -6,6 +6,9 @@ window.images = [
 
 // Write your code here
 let currentIndex = 0;
+let intervalId = null;
+
+// Manual movement of carousel
 function moveFrontOnce(forward) {
     const images = window.images;
     console.log("moveFrontOnce called, forward = ", forward); // Trying to troubleshoot
@@ -21,6 +24,38 @@ function moveFrontOnce(forward) {
     console.log("Image src is set to:", image.src); // Tests shows that my img src is undefined
 }
 
+// Auto movement of carousel forward
+function startAutoForward() {
+    stopAuto(); // Function to stop the autoslide before we start
+    intervalId = setInterval(() => moveFrontOnce(true), 2000); // Starts the autoslide forward every 2"
+    toggleButtons(true); // Auto-forward button active
+}
+
+// Auto movement of carousel backwards
+function startAutoBackward() {
+    stopAuto(); // Stop the autoslide before we start
+    intervalId = setInterval(() => moveFrontOnce(false), 2000); // Starts the autoslide backwards
+    toggleButtons(true); // Auto-backward button active
+}
+
+// Stops automatic slideshow
+function stopAuto() {
+    if (intervalId !== null) {
+        clearInterval(intervalId);
+        intervalId = null;
+    }
+    toggleButtons(false);
+}
+
+// Enables/disables buttons based on if autoslideshow is running
+function toggleButtons(isRunning) {
+    document.getElementById("auto-forward").disabled = isRunning; // When the auto slideshow is on auto-forward button is unclickable
+    document.getElementById("auto-backward").disabled = isRunning; // When auto slideshow is on auto-backward button is unclicable
+    document.getElementById("stop").disabled = !isRunning; // Stop button is disabled only when auto slideshow is off, so when autoslideshow is running
+    //stop is the only button we can click
+}
+
+// Activate (call) the buttons
 document.addEventListener("DOMContentLoaded", () => {
     document
         .getElementById("forward-btn")
@@ -28,4 +63,13 @@ document.addEventListener("DOMContentLoaded", () => {
     document
         .getElementById("backward-btn")
         .addEventListener("click", () => moveFrontOnce(false));
+    document
+        .getElementById("auto-forward")
+        .addEventListener("click", () => startAutoForward());
+    document
+        .getElementById("auto-backward")
+        .addEventListener("click", () => startAutoBackward());
+    document.getElementById("stop").addEventListener("click", () => stopAuto());
+    // Disable stop button initially when autoslideshow is not running yet
+    document.getElementById("stop").disabled = true;
 });
